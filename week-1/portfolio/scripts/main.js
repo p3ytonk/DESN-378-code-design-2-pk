f14e2bbba26bd862f4337ae8d7dfda232e2f83c2
Peyton Whiteley <pknox@ewu.edu>
2026-01-28 17:57:35 -0800

    Update main.js

--- week-1/portfolio/scripts/main.js
+++ week-1/portfolio/scripts/main.js
@@ -2,7 +2,7 @@ const toggle = document.querySelector(".theme-toggle");
 
 document.documentElement.setAttribute("data-theme", "light");
 
-toggle.addEventListener("click", () => {
+toggle.addEventListener("click", function() => {
   const current = document.documentElement.getAttribute("data-theme");
   const next = current === "dark" ? "light" : "dark";
   document.documentElement.setAttribute("data-theme", next);
